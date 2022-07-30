68d85c545f7be293b0d3bf574ad785b858da7794
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-07-30 23:32:13 +0600

    practice add

--- java-script/practice-1.js
+++ java-script/practice-1.js
@@ -1,1 +1,6 @@
-var orange = 1000;
+var mom = 1000;
+var orange = 500;
+var apple = 200;
+var total = orange + apple;
+var value = mom - total;
+console.log(value);
---
+++ java-script/practice-3.js
@@ -0,0 +1,4 @@
+var first = "I am going to be";
+var last = "an awesome web develper"
+var value = first + ' ' + last;
+console.log(value);
