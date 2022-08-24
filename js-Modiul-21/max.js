367db261bc562079870cfb8c0bb6f76ae318ba65
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-08-25 00:18:15 +0600

    new

--- js-Modiul-21/max.js
+++ js-Modiul-21/max.js
@@ -1,7 +1,8 @@
 const jim = 84;
 const dela = 75;
 if (jim > dela) {
-    console.log('jim will get the cake');
+    console.log('true');
+    return true;
 }
 else {
     console.log('dela will get the cake');
